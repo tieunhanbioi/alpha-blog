f82aee2c5455b4705c977695a546a1f82ea3938c
Vincent Ho <doiphongba@gmail.com>
2018-01-11 23:27:16 -0500

    Add secure password and attribute to users

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
+gem 'bcrypt', '~> 3.1.7'
 gem 'bootstrap-sass', '~> 3.3.7'
 gem 'jquery-rails'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
   validates  :email, presence: true, length: { maximum: 105},
              uniqueness: {case_sensitive: false },
              format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  has_secure_password
 end
