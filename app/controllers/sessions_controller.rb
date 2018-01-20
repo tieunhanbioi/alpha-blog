18cdf3e7a9a55afaf27252c2f0329cc631dbe9ba
Vincent Ho <doiphongba@gmail.com>
2018-01-20 15:27:21 -0500

    Updated articles controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-       session[:user_id] = curent_user()
+       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
       redirect_to user_path(user)
       # puts session[:user_id]
